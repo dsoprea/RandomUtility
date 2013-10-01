cc350c0973e4df5ba8a8f96e4bc60bec032b0b64
Dustin Oprea <myselfasunder@gmail.com>
2013-10-01 16:51:37 -0400

    Added password strength-checker.

--- README.txt
+++ README.txt
@@ -9,7 +9,7 @@ get_as_python: Convert data to Python code.
   Example: 
 
     from get_as_python import get_as_python
-
+check_password(complexity_validator, 'simple')
     get_as_python({ 'data1': { 'data22': { 'data33': 44 }}, 
                     'data2': ['aa','bb','cc'], 
                     'data3': ('dd','ee','ff') })
@@ -33,4 +33,15 @@ setup_support: Scripts to help with executable script placement during an
 
     # Same as above, but places in $PREFIX/sbin.
     install_su_tool_symlink('package2.subpackage2.module2')
+
+password_check: A password strength-checker adapted from django-passwords.
+
+    *See example for more information.
+
+    from password_check import ComplexityValidator, ValidationError
+    complexity = { ..complexity parameters.. }
+    complexity_validator = ComplexityValidator(complexity)
+    
+    # Throws ValidationError.
+    complexity_validator('simplepassword')    
 
---
+++ examples/password_check_example.py
@@ -0,0 +1,71 @@
+from password_check import LengthValidator, ComplexityValidator, \
+                           DictionaryValidator, CommonSequenceValidator, \
+                           PASSWORD_COMMON_SEQUENCES, ValidationError
+
+def check_password(validator, password):
+    description = ('%s("%s")' % (validator.__class__.__name__, password))
+
+    try:
+        validator(password)
+    except ValidationError as e:
+        result = str(e)
+    else:
+        result = 'OK'
+
+    print("%s: %s" % (description, result))
+
+length_validator = LengthValidator(8, 15)
+
+complexity = { # A minimum of N upper-case letters.
+               "UPPER": 2,
+               
+               # A minimum of N lower-case letters.
+               "LOWER": 2,
+               
+               # A minimum of N digits.
+               "DIGITS": 2,
+              
+               # A minimum of N punctuation characters.
+               "PUNCTUATION": 2,
+
+               # A minimum of N non-ASCII characters ("\xx")
+               "NON ASCII": 0,
+
+               # A minimum of N space-separated, unique words.
+               "WORDS": 0 }
+
+complexity_validator = ComplexityValidator(complexity)
+
+dictionary_words = ['aaa', 'helloworld', 'mycompanyname', 'mybirthday']
+dictionary_filepath = '/tmp/dictionary.txt'
+with open(dictionary_filepath, 'w') as f:
+    for word in dictionary_words:
+        f.write(word)
+        f.write("\n")
+
+dictionary_validator = DictionaryValidator(dictionary='/tmp/dictionary.txt')
+common_validator = CommonSequenceValidator(PASSWORD_COMMON_SEQUENCES)
+
+print
+check_password(length_validator, 'ab')
+check_password(length_validator, 'abcdefghijklmnop')
+check_password(length_validator, 'abcdefgh')
+
+print
+check_password(complexity_validator, 'simple')
+check_password(complexity_validator, 'simpleAB')
+check_password(complexity_validator, 'simpleAB01')
+check_password(complexity_validator, 'simpleAB01;,')
+
+print
+check_password(dictionary_validator, 'mycompanyname')
+check_password(dictionary_validator, 'mybirthday')
+check_password(dictionary_validator, 'completelyrandompassword')
+
+print
+check_password(common_validator, '12345')
+check_password(common_validator, 'qrstuv')
+check_password(common_validator, 'nocommonsequences')
+
+print
+
