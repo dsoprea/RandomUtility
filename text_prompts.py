4b04a76a1e3c04fe8e3f2c798c1162ca2997b1e7
Dustin Oprea <myselfasunder@gmail.com>
2013-10-10 20:04:15 -0400

    Was incorrectly showing prompt defaults.

--- text_prompts.py
+++ text_prompts.py
@@ -20,6 +20,70 @@ except NameError:
 
 _trunc_length = 10
 
+def _render_prompt(id_, do_space, label_text, is_required, default_value, 
+                   with_nl=False, can_truncate=True):
+    if not default_value:
+        default_value = ''
+        if is_required is False:
+            default_phrase = ' [""]'
+        else:
+            default_phrase = ''
+    else:
+        if can_truncate is True and len(default_value) > _trunc_length:
+            default_value_trunc = default_value[0:_trunc_length] + '...'
+        else:
+            default_value_trunc = default_value
+
+        default_phrase = (' [CTRL+D for "%s"]' %
+                          (default_value_trunc.
+                            replace('\\', '\\\\').
+                            replace('"', '\\"')))
+    
+    if is_required is True and default_phrase == '':
+        required_phrase = ' (req)'
+    else:
+        required_phrase = ''        
+
+    if do_space is True:
+        print('')
+
+    prompt = ('> ' if with_nl is True else '')
+
+    i = 0
+    while 1:
+        stdout.write("%s%s%s:" % 
+                     (label_text, default_phrase, required_phrase))
+        
+        if with_nl is True:
+            stdout.write("\n")
+        else:
+            stdout.write(' ')
+
+        try:
+            answer = raw_input(prompt).strip()
+        except EOFError:
+            """The user aborted the prompt (CTRL+D under Linux)."""
+            print('')
+
+            is_abort = True
+            answer = default_value
+        else:
+            is_abort = False
+
+        if is_required and (is_abort is True and default_value == '' or 
+                            is_abort is False and answer == ''):
+            i += 1
+
+            if i >= 3:
+                raise PromptAbortException((id_, label_text))
+
+            print("\nPlease enter something.\n")
+            continue
+        elif is_required is False and is_abort is True:
+            answer = default_value
+        
+        return (with_nl, answer)
+
 def text_prompts(prompts):
     """Ask for a series of text responses from user. We expect a dictionary
     where the keys are the identifiers used in the resultant dictionary, and  
@@ -31,78 +95,20 @@ def text_prompts(prompts):
     do_space = False
     for id_, spec in prompts.items():
         if len(spec) == 5:
-            (label_text, is_required, default_value, with_nl, 
-             can_truncate_default) = spec
+            (with_nl, answer) = _render_prompt(id_, do_space, *spec)
         else:
-            (label_text, is_required, default_value) = spec
-            with_nl = False
-            can_truncate_default = True
-
-        if default_value is None:
-            default_value = ''
-            if is_required is False:
-                default_phrase = ' [""]'
-            else:
-                default_phrase = ''
-        else:
-            if can_truncate_default is True and \
-               len(default_value) > _trunc_length:
-                default_value_trunc = default_value[0:_trunc_length] + '...'
-            else:
-                default_value_trunc = default_value
-
-            default_phrase = (' [CTRL+D for "%s"]' %
-                              (default_value_trunc.
-                                replace('\\', '\\\\').
-                                replace('"', '\\"')))
-        
-        if is_required is True and default_phrase == '':
-            required_phrase = ' (req)'
-        else:
-            required_phrase = ''        
-
-        if do_space is True:
-            print('')
-            do_space = False
-
-        prompt = ("%s%s%s:" % (label_text, default_phrase, required_phrase))
-
-        if with_nl is True:
-            print(prompt)
-            input_prompt = ''
-        else:
-            input_prompt = prompt + ' '
-
-        i = 0
-        while 1:
-            try:
-                answer = raw_input(input_prompt).strip()
-            except EOFError:
-                """The user aborted the prompt (CTRL+D under Linux)."""
-                print('')
-
-                is_abort = True
-                answer = default_value
-            else:
-                is_abort = False
-
-            if is_required and (is_abort is True and default_value == '' or 
-                                is_abort is False and answer == ''):
-                i += 1
-
-                if i >= 3:
-                    raise PromptAbortException((id_, label_text))
-
-                continue
-            elif is_required is False and is_abort is True:
-                answer = default_value
-            
-            break
+            (with_nl, answer) = _render_prompt(id_, 
+                                               do_space, 
+                                               *spec, 
+                                               with_nl=False, 
+                                               can_truncate=True)
 
         responses[id_] = answer
         j += 1
         if with_nl is True:
             do_space = True
+        else:
+            do_space = False
 
     return responses
 
